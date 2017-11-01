b7aae8c024df485a6a36d8bb1f81d296e940d0f4
reallivedinosaur <joe.hartrick@gmail.com>
2017-11-01 11:17:06 -0400

    audiosettings component

---
+++ webapp/src/app/controlpanel/chichines.audiosettings/controlpanel.audiosettings.ts
@@ -0,0 +1,69 @@
+﻿import { Component } from '@angular/core';
+import { Emulator } from '../../services/NESService';
+import { Observable } from 'rxjs/Observable';
+import { AudioSettings } from 'chichi';
+import { WishboneMachine } from '../../services/wishbone/wishbone';
+
+@Component({
+  selector: 'chichi-audiosettings',
+  templateUrl: './chichi.audiosettings.html',
+  styleUrls: ['./chichi.audiosettings.css']
+})
+
+export class AudioSettingsComponent {
+    audioSettings: AudioSettings = new AudioSettings();
+
+    wishbone: WishboneMachine;
+
+
+    constructor(public nesService: Emulator ) {
+        this.wishbone = nesService.wishbone;
+        this.wishbone.asObservable().subscribe((machine) => {
+            if (machine && machine.SoundBopper) {
+                this.audioSettings = machine.SoundBopper.audioSettings;
+            }
+        });
+    }
+
+    get enableSquare0(): boolean {
+        return this.audioSettings.enableSquare0;
+    }
+
+    set enableSquare0(value: boolean) {
+        this.audioSettings.enableSquare0 = value;
+        this.wishbone.SoundBopper.audioSettings = this.audioSettings;
+        // this.wishbone.RequestSync();
+    }
+
+    get enableSquare1(): boolean {
+        return this.audioSettings.enableSquare1;
+    }
+
+    set enableSquare1(value: boolean) {
+        this.audioSettings.enableSquare1 = value;
+        this.wishbone.SoundBopper.audioSettings = this.audioSettings;
+       // this.wishbone.RequestSync();
+    }
+
+    get enableTriangle(): boolean {
+        return this.audioSettings.enableTriangle;
+    }
+
+    set enableTriangle(value: boolean) {
+        this.audioSettings.enableTriangle = value;
+        this.wishbone.SoundBopper.audioSettings = this.audioSettings;
+        // this.wishbone.RequestSync();
+    }
+
+    get enableNoise(): boolean {
+        return this.audioSettings.enableNoise;
+    }
+
+    set enableNoise(value: boolean) {
+        this.audioSettings.enableNoise = value;
+        this.wishbone.SoundBopper.audioSettings = this.audioSettings;
+       // this.wishbone.RequestSync();
+    }
+
+
+}
